e4f09a0488161f9d0afa51c9356d6ed4a7eeec1e
Karuga Mbugua <karugambugua@icloud.com>
2023-10-08 18:33:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards( namesArray, event ){
+    let thanksYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thanksYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thanksYouCards
+  }
+  function countDown( firstNumber ){
+    while ( firstNumber > 0 ) {
+      console.log( firstNumber );
+      firstNumber -= 1;
+    }
+    console.log( firstNumber );
+  }
